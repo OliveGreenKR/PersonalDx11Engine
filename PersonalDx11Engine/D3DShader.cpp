e01342396ba30e102e25b3fc3f1b7fd1b1918bcc
OliveGreenKR <jeongchul0098@gmail.com>
2025-01-16 16:43:08 +0900

    refact shader::initialize

--- PersonalDx11Engine/D3DShader.cpp
+++ PersonalDx11Engine/D3DShader.cpp
@@ -6,78 +6,48 @@ FD3DShader::~FD3DShader()
 	Shutdown();
 }
 
-bool FD3DShader::Initialize(ID3D11Device* Device, const wchar_t* vertexShaderPath, const wchar_t* pixelShaderPath, D3D11_INPUT_ELEMENT_DESC* layout, unsigned int layoutSize)
+bool FD3DShader::Initialize(ID3D11Device* Device, const wchar_t* vertexShaderPath, const wchar_t* pixelShaderPath, D3D11_INPUT_ELEMENT_DESC* layout, const unsigned int layoutSize)
 {
-	HRESULT result;
+	bool result;
 
-	//Create Shaders
 	ID3DBlob* VertexShaderBlob;
 	ID3DBlob* PixelShaderBlob;
 
-	result = D3DCompileFromFile(vertexShaderPath, nullptr, nullptr, "mainVS", "vs_5_0", 0, 0, &VertexShaderBlob, nullptr);
-	if (FAILED(result))
+	//Create Shaders
+	result = CreateShaders(Device, vertexShaderPath, pixelShaderPath, VertexShaderBlob, PixelShaderBlob);
+	if (!result)
+	{
+		if(VertexShaderBlob)
+			VertexShaderBlob->Release();
+		if(PixelShaderBlob)
+			PixelShaderBlob->Release();
 		return false;
+	}
 
-	result = D3DCompileFromFile(pixelShaderPath, nullptr, nullptr, "mainPS", "ps_5_0", 0, 0, &PixelShaderBlob, nullptr);
-	if (FAILED(result))
-		return false;
-
-	result = Device->CreateVertexShader(VertexShaderBlob->GetBufferPointer(), VertexShaderBlob->GetBufferSize(), nullptr, &VertexShader);
-	if (FAILED(result))
-		return false;
-
-	result = Device->CreatePixelShader(PixelShaderBlob->GetBufferPointer(), PixelShaderBlob->GetBufferSize(), nullptr, &PixelShader);
-	if (FAILED(result))
-		return false;
 
 	//Creatr InputLayout
-	result = Device->CreateInputLayout(layout, layoutSize, VertexShaderBlob->GetBufferPointer(), VertexShaderBlob->GetBufferSize(), &InputLayout);
-	if (FAILED(result))
+	result = CreateInputLayout(Device, VertexShaderBlob, PixelShaderBlob, layout, layoutSize);
+	if (!result)
+	{
+		VertexShaderBlob->Release();
+		PixelShaderBlob->Release();
 		return false;
-
+	}
+		
 
 	//Create ConstantBuffer
-	ID3D11ShaderReflection* Reflector = nullptr;
-	result = D3DReflect(VertexShaderBlob->GetBufferPointer(),VertexShaderBlob->GetBufferSize(),IID_ID3D11ShaderReflection,(void**)&Reflector);
-
-	if (SUCCEEDED(result))
+	result = CreateConstantBuffers(Device, VertexShaderBlob, PixelShaderBlob);
+	if (!result)
 	{
-		D3D11_SHADER_DESC ShaderDesc;
-		Reflector->GetDesc(&ShaderDesc);
-
-		// 상수 버퍼 생성
-		for (unsigned int i = 0; i < ShaderDesc.ConstantBuffers; i++)
-		{
-			ID3D11ShaderReflectionConstantBuffer* CBReflection =
-				Reflector->GetConstantBufferByIndex(i);
-
-			D3D11_SHADER_BUFFER_DESC BufferDesc;
-			CBReflection->GetDesc(&BufferDesc);
-
-			D3D11_BUFFER_DESC ConstantBufferDesc = {};
-			ConstantBufferDesc.ByteWidth = BufferDesc.Size;
-			ConstantBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
-			ConstantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
-			ConstantBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
-			ConstantBufferDesc.MiscFlags = 0;
-			ConstantBufferDesc.StructureByteStride = 0;
-
-			ID3D11Buffer* ConstantBuffer = nullptr;
-			result = Device->CreateBuffer(&ConstantBufferDesc, nullptr, &ConstantBuffer);
-			if (FAILED(result))
-				return false;
-			else
-			{
-				ConstantBuffers.push_back(ConstantBuffer);
-			}
-		}
-		Reflector->Release();
+		VertexShaderBlob->Release();
+		PixelShaderBlob->Release();
+		return false;
 	}
 
 	VertexShaderBlob->Release();
 	PixelShaderBlob->Release();
 
-	return true;
+	return result;
 }
 
 void FD3DShader::Shutdown()
@@ -120,9 +90,82 @@ void FD3DShader::Bind(ID3D11DeviceContext* DeviceContext)
 
 void FD3DShader::SetTexture(ID3D11DeviceContext* DeviceContext, ID3D11ShaderResourceView* TextureView)
 {
+	return;
 }
 
 void FD3DShader::SetSamplerState(ID3D11DeviceContext* DeviceContext, ID3D11SamplerState* SamplerState)
 {
+	return;
 }
 
+bool FD3DShader::CreateShaders(ID3D11Device* Device, const wchar_t* vertexShaderPath, const wchar_t* pixelShaderPath, OUT ID3DBlob* VSBlob, OUT ID3DBlob* PSBlob)
+{
+	HRESULT result;
+	result = D3DCompileFromFile(vertexShaderPath, nullptr, nullptr, "mainVS", "vs_5_0", 0, 0, &VSBlob, nullptr);
+	if (FAILED(result))
+		return false;
+
+	result = D3DCompileFromFile(pixelShaderPath, nullptr, nullptr, "mainPS", "ps_5_0", 0, 0, &PSBlob, nullptr);
+	if (FAILED(result))
+		return false;
+
+	result = Device->CreateVertexShader(VSBlob->GetBufferPointer(), VSBlob->GetBufferSize(), nullptr, &VertexShader);
+	if (FAILED(result))
+		return false;
+
+	result = Device->CreatePixelShader(PSBlob->GetBufferPointer(), PSBlob->GetBufferSize(), nullptr, &PixelShader);
+	if (FAILED(result))
+		return false;
+	return true;
+}
+
+bool FD3DShader::CreateInputLayout(ID3D11Device* Device, ID3DBlob* VSBlob, ID3DBlob* PSBlob, D3D11_INPUT_ELEMENT_DESC* layout, unsigned int layoutSize)
+{
+	return (SUCCEEDED(Device->CreateInputLayout
+			(layout, layoutSize,VSBlob->GetBufferPointer(),PSBlob->GetBufferSize(),&InputLayout)));
+}
+
+bool FD3DShader::CreateConstantBuffers(ID3D11Device* Device, ID3DBlob* VSBlob, ID3DBlob* PSBlob)
+{
+	HRESULT result;
+
+
+	ID3D11ShaderReflection* Reflector = nullptr;
+	result = D3DReflect(VSBlob->GetBufferPointer(), VSBlob->GetBufferSize(), IID_ID3D11ShaderReflection, (void**)&Reflector);
+
+	if (SUCCEEDED(result))
+	{
+		D3D11_SHADER_DESC ShaderDesc;
+		Reflector->GetDesc(&ShaderDesc);
+
+		// 상수 버퍼 생성
+		for (unsigned int i = 0; i < ShaderDesc.ConstantBuffers; i++)
+		{
+			ID3D11ShaderReflectionConstantBuffer* CBReflection =
+				Reflector->GetConstantBufferByIndex(i);
+
+			D3D11_SHADER_BUFFER_DESC BufferDesc;
+			CBReflection->GetDesc(&BufferDesc);
+
+			D3D11_BUFFER_DESC ConstantBufferDesc = {};
+			ConstantBufferDesc.ByteWidth = BufferDesc.Size;
+			ConstantBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
+			ConstantBufferDesc.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+			ConstantBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+			ConstantBufferDesc.MiscFlags = 0;
+			ConstantBufferDesc.StructureByteStride = 0;
+
+			ID3D11Buffer* ConstantBuffer = nullptr;
+			result = Device->CreateBuffer(&ConstantBufferDesc, nullptr, &ConstantBuffer);
+			if (FAILED(result))
+				return false;
+			else
+			{
+				ConstantBuffers.push_back(ConstantBuffer);
+			}
+		}
+		Reflector->Release();
+	}
+
+	return true;
+}
--- PersonalDx11Engine/D3DShader.h
+++ PersonalDx11Engine/D3DShader.h
@@ -11,23 +11,25 @@ public:
 	~FD3DShader();
 
 public:
-	bool Initialize(ID3D11Device* Device, const wchar_t* vertexShaderPath, const wchar_t* pixelShaderPath, D3D11_INPUT_ELEMENT_DESC* layout, unsigned int layoutSize);
+	bool Initialize(ID3D11Device* Device, const wchar_t* vertexShaderPath, const wchar_t* pixelShaderPath, D3D11_INPUT_ELEMENT_DESC* layout, const unsigned int layoutSize);
 	void Shutdown();
 
 	void Bind(ID3D11DeviceContext* DeviceContext);
-
 
 	template<typename T>
 	void UpdateConstantBuffer(ID3D11DeviceContext* DeviceContext,const T& BufferData, unsigned int BufferIndex = 0);
 	void SetTexture(ID3D11DeviceContext* DeviceContext,ID3D11ShaderResourceView* TextureView);
 	void SetSamplerState(ID3D11DeviceContext* DeviceContext,ID3D11SamplerState* SamplerState);
-	
+
+private:
+	bool CreateShaders(ID3D11Device* Device, const wchar_t* vertexShaderPath, const wchar_t* pixelShaderPath, OUT ID3DBlob* VSBlob, OUT ID3DBlob* PSBlob);
+	bool CreateInputLayout(ID3D11Device* Device, ID3DBlob* VSBlob, ID3DBlob* PSBlob, D3D11_INPUT_ELEMENT_DESC* layout, unsigned int layoutSize);
+	bool CreateConstantBuffers(ID3D11Device* Device, ID3DBlob* VSBlob, ID3DBlob* PSBlob);
 private:
 	ID3D11VertexShader* VertexShader = nullptr;
 	ID3D11PixelShader* PixelShader = nullptr;
 	ID3D11InputLayout* InputLayout = nullptr;
 	vector<ID3D11Buffer*> ConstantBuffers;
-
 };
 
 template<typename T>
@@ -45,6 +47,7 @@ inline void FD3DShader::UpdateConstantBuffer(ID3D11DeviceContext* DeviceContext,
 
 	if (FAILED(result))
 		return;
+
 	//from CPU to GPU memory
 	memcpy(MappedResource.pData, &BufferData, sizeof(T));
 	DeviceContext->Unmap(ConstantBuffers[BufferIndex], 0);
