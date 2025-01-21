83d18cc84eb14db5e0d9397d28d619411e832ed4
OliveGreenKR <jeongchul0098@gmail.com>
2025-01-21 09:43:06 +0900

    delete Math.h

---
+++ PersonalDx11Engine/Camera.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <directxmath.h>
+
+using namespace DirectX;
+
+class UCamera
+{
+private:
+	XMFLOAT3 Position;
+
+};
+
--- PersonalDx11Engine/Math.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <cmath>
-#include <directxmath.h>
-
-template <typename T>
-const T& clamp(const T& v, const T& lo, const T& hi) {
-    return (v < lo) ? lo : (hi < v) ? hi : v;
-}
